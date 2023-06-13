87e77b7645504918b337018bb91771911c0f9564
Shukurah <shukkike@gmail.com>
2023-06-13 08:24:04 +0100

    Calendar updated

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -1,7 +1,20 @@
+"use client";
 import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 const Calendar = () => {
-  return <div>Coming soon</div>;
+  return (
+    <FullCalendar
+      plugins={[dayGridPlugin, interactionPlugin]}
+      initialView="dayGridMonth"
+      editable={true}
+      droppable={true}
+      dayMaxEvents={true}
+      events="https://fullcalendar.io/api/demo-feeds/events.json?overload-day"
+    />
+  );
 };
 
 export default Calendar;
